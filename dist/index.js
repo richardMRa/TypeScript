6a3ff04fc9746bac2d572ce776e85dd6471294f8
richardMRa <richete05@gmail.com>
2022-08-23 19:53:08 -0500

    Enum

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,6 @@
 let numbers = [0, 1, 2, 3];
 let strings = ['H', 'O', 'L', 'A'];
 let user = [1, 'Me'];
-let mySize = 4;
+let mySize = 2;
 console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,23 @@
 
 //Arrays
 
-//Number type Array
+    //Number type Array
 let numbers: number[] = [0,1,2,3]
 
-//String type Array
+    //String type Array
 let strings: string[] = ['H', 'O', 'L', 'A']
 
 //Tuples - Fixed length Array
 
-//This will be compiled as a normal Array
-//Push method is not checked by typescript
-//Key value pairs use recommended 
+    //This will be compiled as a normal Array
+    //Push method is not checked by typescript
+    //Key value pairs use recommended 
 
 let user: [number, string] = [1, 'Me'];
 
 //Enum - group of related variables
 
-//Use const to produce a better code
+    //Use const to generate more optimized code
 
 const enum Size {Small = 1, Medium, Large}
 let mySize = Size.Medium;
