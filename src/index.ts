927390ca6728be5243a0c9be2f68fc33eb554f3e
richardMRa <richete05@gmail.com>
2022-08-23 19:44:43 -0500

    Tuples

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,15 @@
 //Arrays
 
 //Number type Array
-let numbers : number[] = [0,1,2,3]
+let numbers: number[] = [0,1,2,3]
 
 //String type Array
-let strings : string[] = ['H', 'O', 'L', 'A']
+let strings: string[] = ['H', 'O', 'L', 'A']
+
+//Tuples - Fixed length Array
+//This will be compiled as a normal Array
+//Push method is not checked by typescript
+//Key value pairs use recommended 
+
+let user: [number, string] = [1, 'Me'];
+
